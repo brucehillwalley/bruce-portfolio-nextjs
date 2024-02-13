462ffc4bb8b6acc7a716b8b7e4e12cea3cc1769c
BruceHill <brucehillwalley@gmail.com>
2024-02-13 22:38:31 +0300

    signature

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -30,13 +30,36 @@ const AboutPage = () => {
             {/* BIOGRAPHY SIGN */}
             <div className="self-end">
               <Image
-                className="-mt-36"
+                className=""
                 src="/signature.png"
                 alt="signature"
-                width={400}
-                height={400}
+                width={242}
+                height={40}
               />
             </div>
+            {/* BIOGRAPHY SCROLL SVG */}
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
 
           {/* SKILLS CONTAINER */}
@@ -44,15 +67,89 @@ const AboutPage = () => {
             {/* SKILL TITLE */}
             <h1 className="font-bold text-2xl text-sky-900"> SKILL </h1>
             {/* SKILL LIST */}
-          <div className=""></div>
+
+            <div className="flex gap-4 flex-wrap">
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Javascript
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                React.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                TypeScript
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Next.js
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Tailwind CSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                SCSS
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Material UI
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Cypress
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Context API
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Redux / Redux Toolkit
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Firebase
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                Contentful
+              </div>
+              <div className="rounded p-2 text-sm cursor-pointer bg-sky-900 text-white hover:bg-white hover:text-sky-900">
+                REST API
+              </div>
+            </div>
             {/* SKILL SCROLL SVG */}
-            
-          
-          
+            <motion.svg
+              initial={{ opacity: 0.2, y: 0 }}
+              animate={{ opacity: 1, y: "10px" }}
+              transition={{ repeat: Infinity, duration: 3, ease: "easeInOut" }}
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              width={50}
+              height={50}
+            >
+              <path
+                d="M5 15C5 16.8565 5.73754 18.6371 7.05029 19.9498C8.36305 21.2626 10.1435 21.9999 12 21.9999C13.8565 21.9999 15.637 21.2626 16.9498 19.9498C18.2625 18.6371 19 16.8565 19 15V9C19 7.14348 18.2625 5.36305 16.9498 4.05029C15.637 2.73754 13.8565 2 12 2C10.1435 2 8.36305 2.73754 7.05029 4.05029C5.73754 5.36305 5 7.14348 5 9V15Z"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+              <path d="M12 6V14" stroke="#000000" strokeWidth="1"></path>
+              <path
+                d="M15 11L12 14L9 11"
+                stroke="#000000"
+                strokeWidth="1"
+              ></path>
+            </motion.svg>
           </div>
 
           {/* EXPERIENCE CONTAINER */}
-          <div className="">EXPERIENCE</div>
+          <div className="flex flex-col gap-12 justify-center pb-48">
+            {/* EXPERIENCE TITLE */}
+            <h1 className="font-bold text-2xl text-sky-900"> EXPERIENCE </h1>
+            {/* EXPERIENCE LIST */}
+            <div>
+              {/* EXPERIENCE LIST ITEM */}
+              <div className="">
+                {/* LEFT */}
+
+                {/* CENTER */}
+                
+                {/* RIGHT */}
+              </div>
+            </div>
+          </div>
         </div>
         {/* SVG CONTAINER */}
         <div></div>
